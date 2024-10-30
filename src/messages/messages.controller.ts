8002a8601fea6af2ce8db72b573de691f1dc00ce
Usman Afzal <109211422+usmannafzal@users.noreply.github.com>
2024-10-30 20:30:53 +0500

    JSON parsing logic for messages added

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,27 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages(): string[] {
-    return ['message 1', 'message 2'];
+  listMessages() {
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
