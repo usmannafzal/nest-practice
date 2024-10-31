b2d5002835363c14813448a613c272182e15ceca
Usman Afzal <109211422+usmannafzal@users.noreply.github.com>
2024-10-31 18:55:42 +0500

    Powers app added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { MessagesModule } from './messages/messages.module';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
+  const app = await NestFactory.create(ComputerModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
